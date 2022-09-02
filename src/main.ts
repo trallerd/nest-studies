16f98407f3792d70745e3697ad18fb02a65cf983
trallerd <jessicacristec@gmail.com>
2022-09-02 11:05:37 -0300

    hbs partials

--- src/main.ts
+++ src/main.ts
@@ -1,12 +1,16 @@
 import { NestFactory } from '@nestjs/core';
-import { AppModule } from './app.module';
 import { NestExpressApplication } from '@nestjs/platform-express';
 import { join } from 'path';
+import { AppModule } from './app.module';
+import * as hbs from 'hbs';
+import * as hbsUtils from 'hbs-utils';
 
 async function bootstrap() {
   const app = await NestFactory.create<NestExpressApplication>(AppModule);
   app.useStaticAssets(join(__dirname, '..', 'public'));
   app.setBaseViewsDir(join(__dirname, '..', 'view'));
+  hbs.registerPartials(join(__dirname, '..', 'view/layouts'));
+  hbsUtils(hbs).registerWatchedPartials(join(__dirname, '..', 'view/layouts'));
   app.setViewEngine('hbs');
   await app.listen(3000);  
 }
