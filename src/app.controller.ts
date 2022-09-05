1984af3f9caf3d975e32ecb0b3203dc1890b5ab8
trallerd <jessicacristec@gmail.com>
2022-09-05 16:49:07 -0300

    controller

--- src/app.controller.ts
+++ src/app.controller.ts
@@ -4,5 +4,23 @@ import { Controller, Get, Render } from "@nestjs/common";
 export class AppController{
   @Get("/")
   @Render('index')
-  index(){}
+  index(){
+    return {
+      title: "Home Page - Online Store"
+    };
+  }
+
+  @Get("/about")
+  @Render('about')
+  about(){
+    let viewData = [];
+    viewData["description"] = "This is an about page ..."
+    viewData["author"] = "Developed by: Jéss Gonçalves"
+    let data1 = 'About us - Online Store'
+    return {
+      title: data1,
+      subtitle: "About us",
+      viewData: viewData
+    };
+  }
 }
