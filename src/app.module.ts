eee71264bae7d322e73984b01c922203e41a43b6
trallerd <jessicacristec@gmail.com>
2022-09-06 16:52:21 -0300

    get products from database

--- src/app.module.ts
+++ src/app.module.ts
@@ -2,22 +2,23 @@ import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { ProductsController } from './products.controller';
 import { TypeOrmModule } from '@nestjs/typeorm';
+import { Product } from './models/product.entity';
+import { ProductService } from './models/products.service';
 
 @Module({
   imports: [
     TypeOrmModule.forRoot({
-      "type": "mysql",
+      "type": "mariadb",
       "host": "localhost",
       "username": "root",
       "password": "",
       "database": "online_store",
-      "entities": ["dist/**/*.entities{.ts,.js}"],
+      "entities": ["dist/**/*.entity{.ts,.js}"],
       "synchronize": true,
-      "logging": true,
-      "subscribers": [],
-      "migrations": []
   }),
+  TypeOrmModule.forFeature([Product]),
   ],
   controllers: [AppController, ProductsController],
+  providers: [ProductService],
 })
 export class AppModule {}
--- src/products.controller.ts
+++ src/products.controller.ts
@@ -1,60 +1,32 @@
-import { Controller, Get, Param, Render } from '@nestjs/common';
+import { Controller, Get, Logger, Param, Render, Res } from '@nestjs/common';
+import { ProductService } from './models/products.service';
 
 @Controller('/products')
 export class ProductsController{
-    static products = [
-        {
-            id: '1',
-            name: 'TV', 
-            desciption: 'Best tv',
-            image: 'game.png',
-            price: '1000',
-        },
-        {
-            id: '2',
-            name: 'Iphone', 
-            desciption: 'Best Iphone',
-            image: 'safe.png',
-            price: '999',
-        },
-        {
-            id: '3',
-            name: 'ChromeCast', 
-            desciption: 'Best ChromeCast',
-            image: 'submarine.png',
-            price: '30',
-        },
-        {
-            id: '4',
-            name: 'Glasses', 
-            desciption: 'Best Glasses',
-            image: 'game.png',
-            price: '100',
-        },
-    ];
-
+    constructor(private readonly productService: ProductService){}
+    
     @Get('/')
     @Render('products/index')
-    index(){
+    async index(){
         const viewData = [];
         viewData['title'] = 'Products - Oline Store';
         viewData['subtitle'] = 'List of Products';
-        viewData['products'] = ProductsController.products;
+        viewData['products'] = await this.productService.findAll();
         return {
             viewData: viewData,
         };
     }
 
     @Get('/:id')
-    @Render('products/show')
-    show(@Param() params){
-        const product = ProductsController.products[params.id - 1];
+    async show(@Param() params, @Res() response){
+        const product = await this.productService.findOne(params.id);
+        if (product === null){
+            return response.redirect('/products');
+        }
         const viewData = [];
         viewData['title'] = product.name + ' - Online Store';
         viewData['subtitle'] = product.name + ' - Product Information';
         viewData['product'] = product;
-        return {
-            viewData: viewData,
-        };
+        return response.render('products/show', {viewData:viewData});
     }
 }
