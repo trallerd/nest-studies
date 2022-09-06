9ac43dfdfe384abc4fb9e923566e451db6684a5c
trallerd <jessicacristec@gmail.com>
2022-09-06 15:09:42 -0300

    setup db

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,9 +1,23 @@
 import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { ProductsController } from './products.controller';
+import { TypeOrmModule } from '@nestjs/typeorm';
 
 @Module({
-  imports: [],
+  imports: [
+    TypeOrmModule.forRoot({
+      "type": "mysql",
+      "host": "localhost",
+      "username": "root",
+      "password": "",
+      "database": "online_store",
+      "entities": ["dist/**/*.entities{.ts,.js}"],
+      "synchronize": true,
+      "logging": true,
+      "subscribers": [],
+      "migrations": []
+  }),
+  ],
   controllers: [AppController, ProductsController],
 })
 export class AppModule {}
