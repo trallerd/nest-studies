8dc74fc5cf0fc9a91aac1c686ce9d69461874067
trallerd <jessicacristec@gmail.com>
2022-09-21 18:32:18 -0300

    delete product

--- src/admin/admin.products.controller.ts
+++ src/admin/admin.products.controller.ts
@@ -1,4 +1,4 @@
-import { Body, Controller, Get, Post, Redirect, Render, UseInterceptors, UploadedFile } from "@nestjs/common";
+import { Body, Controller, Get, Post, Redirect, Render, UseInterceptors, UploadedFile, Param } from "@nestjs/common";
 import { FileInterceptor } from "@nestjs/platform-express";
 import { Product } from "../models/product.entity";
 import { ProductService } from "../models/products.service";
@@ -19,14 +19,20 @@ export class AdminProductsController {
     }
 
     @Post('/store')
-    @UseInterceptors(FileInterceptor('image', {dest: './public/uploads'}))
+    @UseInterceptors(FileInterceptor('image', { dest: './public/uploads' }))
     @Redirect('/admin/products')
-    async store(@Body() body, @UploadedFile() file: Express.Multer.File){
+    async store(@Body() body, @UploadedFile() file: Express.Multer.File) {
         const newProduct = new Product();
         newProduct.setName(body.name);
         newProduct.setDescription(body.description);
         newProduct.setPrice(body.price);
         newProduct.setImage(file.filename);
         await this.productService.createOrUpdate(newProduct);
+    }
+
+    @Post('/:id')
+    @Redirect('admin/products')
+    remove(@Param('id') id: string) {
+        return this.productService.remove(id);
     }
 }
--- src/models/products.service.ts
+++ src/models/products.service.ts
@@ -1,24 +1,28 @@
 import { Injectable } from "@nestjs/common";
 import { InjectRepository } from "@nestjs/typeorm";
 import { Repository } from "typeorm";
-import { Product} from "./product.entity";
+import { Product } from "./product.entity";
 
 @Injectable()
-export class ProductService{
+export class ProductService {
     constructor(
         @InjectRepository(Product)
         private productsRepository: Repository<Product>,
-    ){}
+    ) { }
 
     findAll(): Promise<Product[]> {
         return this.productsRepository.find();
     }
 
-    findOne(id: string): Promise<Product>{
-        return this.productsRepository.findOne({where:{id:parseInt(id)}});
+    findOne(id: string): Promise<Product> {
+        return this.productsRepository.findOne({ where: { id: parseInt(id) } });
     }
 
-    createOrUpdate(product: Product): Promise<Product>{
+    createOrUpdate(product: Product): Promise<Product> {
         return this.productsRepository.save(product);
+    }
+
+    async remove(id: string): Promise<void> {
+        await this.productsRepository.delete(id);
     }
 }
