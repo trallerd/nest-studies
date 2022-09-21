d13456cd99d755fd920a1f401ece1eac50530829
trallerd <jessicacristec@gmail.com>
2022-09-21 20:33:05 -0300

    add user entity

---
+++ src/models/user.entity.ts
@@ -0,0 +1,66 @@
+import { Column, Entity, PrimaryGeneratedColumn } from "typeorm";
+
+@Entity()
+export class User {
+    @PrimaryGeneratedColumn()
+    id: number;
+
+    @Column()
+    name: string;
+
+    @Column({ unique: true })
+    email: string;
+
+    @Column()
+    password: string;
+
+    @Column()
+    role: string;
+
+    @Column()
+    balance: number;
+
+    getId(): number {
+        return this.id;
+    }
+
+    setId(id: number) {
+        this.id = id;
+    }
+
+    getName(): string {
+        return this.name;
+    }
+
+    setName(name: string) {
+        this.name = name;
+    }
+
+    getEmail(): string {
+        return this.email;
+    }
+
+    setEmail(email: string) {
+        this.email = email;
+    }
+
+    getPassword(): string {
+        return this.password;
+    }
+
+    setPassword(password: string) {
+        this.password = password;
+    }
+
+    getRole(): string {
+        return this.role;
+    }
+
+    getBalance(balance: number) {
+        return this.balance;
+    }
+
+    setBalance(balance: number) {
+        this.balance = balance;
+    }
+}
