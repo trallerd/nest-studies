a4ab72f593d238e8f7c5b5a33a71266c91583c7c
trallerd <jessicacristec@gmail.com>
2022-09-21 20:20:17 -0300

    complete CRUD of products

--- src/admin/admin.products.controller.ts
+++ src/admin/admin.products.controller.ts
@@ -35,4 +35,33 @@ export class AdminProductsController {
     remove(@Param('id') id: string) {
         return this.productService.remove(id);
     }
+
+    @Get('/:id')
+    @Render('admin/products/edit')
+    async edit(@Param('id') id: string) {
+        const viewData = [];
+        viewData['title'] = 'Admin Page - Edit Product - Online'
+        viewData['product'] = await this.productService.findOne(id);
+        return {
+            viewData: viewData,
+        };
+    }
+
+    @Post('/:id/update')
+    @UseInterceptors(FileInterceptor('image', { dest: './public/uploads' }))
+    @Redirect('/admin/products')
+    async update(
+        @Body() body,
+        @UploadedFile() file: Express.Multer.File,
+        @Param('id') id: string,
+    ) {
+        const product = await this.productService.findOne(id);
+        product.setName(body.name);
+        product.setDescription(body.description);
+        product.setPrice(body.price);
+        if (file) {
+            product.setImage(file.filename);
+        }
+        await this.productService.createOrUpdate(product);
+    }
 }
